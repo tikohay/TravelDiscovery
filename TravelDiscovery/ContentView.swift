fd957cf1b98dba8b151a3041757aeb975d5b2cca
Tigran Karakhanyan <lev.98@bk.ru>
2022-04-21 18:36:13 +0400

    added Category

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+struct Category: Identifiable {
+    var id = UUID()
+    let name, imageName: String
+}
+
 struct ContentView: View {
     var body: some View {
         NavigationView {
@@ -14,6 +19,7 @@ struct ContentView: View {
                 DiscoverCategoriesView()
                 PopularDestinationView()
                 PopularRestaurantsView()
+                TrendingCreatorsView()
             } .navigationTitle("Discover")
         }
     }
@@ -22,25 +28,39 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice((PreviewDevice(rawValue: "iPhone 12 Pro Max")))
     }
 }
 
 struct DiscoverCategoriesView: View {
+    
+    let categories: [Category] = [
+        .init(name: "Art", imageName: "paintpalette.fill"),
+        .init(name: "Sport", imageName: "sportscourt.fill"),
+        .init(name: "Live Events", imageName: "music.mic"),
+        .init(name: "Food", imageName: "music.mic"),
+        .init(name: "History", imageName: "music.mic")
+    ]
+    
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 8) {
-                ForEach(0..<5, id: \.self) { _ in
-                    VStack(spacing: 4) {
-                        Spacer()
-                            .frame(width: 50, height: 50)
-                            .background(.yellow)
+            HStack(alignment: .top, spacing: 14) {
+                ForEach(categories, id: \.id) { category in
+                    VStack(spacing: 8) {
+                        Image(systemName: category.imageName)
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .frame(width: 64, height: 64)
+                            .background(.gray)
                             .cornerRadius(.infinity)
                             .shadow(color: .gray, radius: 5, x: 0, y: 2)
-                        Text("Art")
+                        Text(category.name)
                             .font(.system(size: 12, weight: .semibold))
+                            .multilineTextAlignment(.center)
                     }
+                    .frame(width: 68)
                 }
-            }.padding(.horizontal)
+            }   .padding(.horizontal)
                 .padding(.top, 5)
         }
     }
@@ -104,7 +124,7 @@ struct TrendingCreatorsView: View {
     var body: some View {
         VStack {
             HStack {
-                Text("Pupular places to eat")
+                Text("Trending creators")
                     .font(.system(size: 14, weight: .semibold))
                 Spacer()
                 Text("See all")
@@ -115,13 +135,13 @@ struct TrendingCreatorsView: View {
                 HStack(spacing: 8.0) {
                     ForEach(0..<5, id: \.self) { num in
                         Spacer()
-                            .frame(width: 200, height: 64)
-                            .background(.gray)
-                            .cornerRadius(5)
-                            .shadow(color: .gray, radius: 4, x: 0, y: 2)
+                            .frame(width: 50, height: 50)
+                            .background(.yellow)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 5, x: 0, y: 2)
                             .padding(.bottom)
                     }
-                } .padding(.horizontal)
+                } .padding([.horizontal, .top])
             }
         }
     }
